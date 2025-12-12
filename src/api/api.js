4b59dd2593788ee814d94d4d5b53289aeff32458
Trần Văn Giáp <gipvosongf@gmail.com>
2025-12-12 10:15:46 +0700

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -5,4 +5,12 @@ const api = axios.create({
   timeout: 5000,
 })
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
 export default api
